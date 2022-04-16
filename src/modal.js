476d8787e1734b2246510eef8f61d47927c1114b
Gunbam27 <mingmi_27@naver.com>
2022-04-16 16:15:58 +0900

    04.16
    #page2 css & data

--- src/modal.js
+++ src/modal.js
@@ -1,63 +1,58 @@
-// Modal을 가져옵니다.
-const modals = document.getElementsByClassName('modal');
-// Modal을 띄우는 클래스 이름을 가져옵니다.
-const btns = document.getElementsByClassName('btn');
-// Modal을 닫는 close 클래스를 가져옵니다.
-const spanes = document.getElementsByClassName('close');
+// 서브페이지로 이동하는 버튼의 클래스 이름을 가져옵니다.
+const btns = document.getElementsByClassName("btn");
 
 // 클릭하여 내용 바꾸기
-const gallery = document.querySelector('.gallery_container');
-const title = document.querySelector('.feetxt h1');
-const mimg = document.querySelector('.header-img');
-const summary = document.querySelector('.feetxt span');
-const desc = document.querySelectorAll('.feetxt p');
-const memberfee = document.querySelectorAll('.memberfee p');
-
-const before = document.querySelector('.before');
-const sub = document.querySelector('.sub-container');
+const gallery = document.querySelector(".gallery_container");
+const title = document.querySelector(".feetxt h1");
+const mimg = document.querySelector(".sub-img");
+const summary = document.querySelector(".feetxt span");
+const desc = document.querySelectorAll(".feetxt p");
+const memberfee = document.querySelectorAll(".memberfee p");
+const before = document.querySelector(".before");
+const sub1 = document.querySelector(".sub-container");
+const sub2_txt = document.querySelector(".sub2-txt");
+const sub2_li = document.querySelectorAll(".sub2-img-box li img");
 let funcs = [];
-
 
 // Modal을 띄우고 닫는 클릭 이벤트를 정의한 함수
 function Modal(num) {
   return function () {
     // 해당 클래스의 내용을 클릭하면 Modal을 띄웁니다.
     btns[num].onclick = function () {
-      
-
       // data-id 출력
       let id = btns[num].dataset.id;
-      console.log('data-id', id);
+      console.log("data-id", id);
       // id에 따른 객체 데이터 읽어오기
       let data = deck[id];
-      console.log('데이터:', data);
+      console.log("데이터:", data);
 
       // data-id에따라 바뀌는 내용
-      mimg.style.backgroundImage =`url(images/${id}all.png)`;
-      title.innerText = data['타이틀'];
-      summary.innerText = data['요약'];
-      data['설명'].forEach((val, idx) => {
+      mimg.style.backgroundImage = `url(images/${id}all.png)`;
+      title.innerText = data["타이틀"];
+      summary.innerText = data["요약"];
+      sub2_txt.innerText = `${data["타이틀"]}의
+      특별한 디자인을 소개합니다.`;
+      data["설명"].forEach((val, idx) => {
         desc[idx].innerText = val;
       });
-      data['요금'].forEach((val, idx) => {
+      data["요금"].forEach((val, idx) => {
         memberfee[idx].innerText = val;
       });
-      ////////// 숨기고 보이기  //////////
-      gallery.style.display = 'none';
-      cardname.style.display = 'none';
-      sub.style = 'display:block';
-      before.style='display:block';
-      before.addEventListener('click', () => {
-        before.style='display:none';
-        sub.style = 'display:none';
-        gallery.style.display = 'flex';
-        cardname.style.display = 'block';
+      data["디자인"].forEach((val, idx) => {
+        sub2_li[idx].src = `images/${val}`;
       });
 
-      // <span> 태그(X 버튼)를 클릭하면 Modal이 닫습니다.
-      // spanes[num].onclick = () => {
-      //   modals[num].style.display = 'none';
-      // };
+      ////////// 숨기고 보이기  //////////
+      gallery.style.display = "none";
+      cardname.style.display = "none";
+      sub1.style = "display:block";
+      before.style = "display:block";
+      before.addEventListener("click", () => {
+        before.style = "display:none";
+        sub1.style = "display:none";
+        gallery.style.display = "flex";
+        cardname.style.display = "block";
+      });
     };
   };
 }
@@ -76,69 +71,126 @@ for (var j = 0; j < btns.length; j++) {
 
 var deck = {
   green: {
-    타이틀: 'the Green Edition2',
-    요약: 'My First Luxury',
+    타이틀: "the Green Edition2",
+    요약: "My First Luxury",
     설명: [
-      '자유와 모험을 즐기는, 나의 첫 럭셔리 카드',
-      '국내외 여행·해외쇼핑 5% 적립',
+      "자유와 모험을 즐기는, 나의 첫 럭셔리 카드",
+      "국내외 여행·해외쇼핑 5% 적립",
     ],
-    요금:['본인 150,000원','본인 150,000원','본인 150,000원'],
+    요금: ["본인 150,000원", "본인 150,000원", "본인 150,000원"],
+    디자인: [
+      "green1.png",
+      "green2.png",
+      "green3.png",
+      "green4.png",
+      "green5.png",
+    ],
   },
   red: {
-    타이틀: 'the Red Edition5',
-    요약: 'My Hot Luxury',
-    설명: ['럭셔리 라이프스타일을 매순간 더 핫하게', '20만원 프리미엄 바우처'],
-    요금:['본인 300,000원','본인 300,000원','본인 300,000원'],
+    타이틀: "the Red Edition5",
+    요약: "My Hot Luxury",
+    설명: ["럭셔리 라이프스타일을 매순간 더 핫하게", "20만원 프리미엄 바우처"],
+    요금: ["본인 300,000원", "본인 300,000원", "본인 300,000원"],
+    디자인: ["red1.png", "red2.png", "red3.png", "red4.png", "red5.png"],
   },
   pink: {
-    타이틀: 'the Pink',
-    요약: 'My First Seduction',
-    설명: ['프로쇼퍼들의 필수 럭셔리 카드', '프리미엄쇼핑·고메 5% 적립'],
-    요금:['본인 150,000원','본인 150,000원','본인 150,000원'],
+    타이틀: "the Pink",
+    요약: "My First Seduction",
+    설명: ["프로쇼퍼들의 필수 럭셔리 카드", "프리미엄쇼핑·고메 5% 적립"],
+    요금: ["본인 150,000원", "본인 150,000원", "본인 150,000원"],
+    디자인: ["pink1.png", "pink2.png", "pink3.png", "pink4.png", "pink5.png"],
   },
   purple: {
-    타이틀: 'the Purple osée',
-    요약: 'Not Just Luxury',
+    타이틀: "the Purple osée",
+    요약: "Not Just Luxury",
     설명: [
-      '취향따라 설계·사용하는 바우처 네트워크',
-      '항공마일리지와 M포인트 중 선택 적립 기본 적립',
-      '혜택에 추가로 제공되는 로열티 보너스',
+      "취향따라 설계·사용하는 바우처 네트워크",
+      "항공마일리지와 M포인트 중 선택 적립 기본 적립",
+      "혜택에 추가로 제공되는 로열티 보너스",
     ],
-    요금:['본인 800,000원','본인 800,000원','본인 800,000원'],
+    요금: ["본인 800,000원", "본인 800,000원", "본인 800,000원"],
+    디자인: ["viol1.png", "viol2.png", "viol3.png"],
   },
   fam: {
-    타이틀: '현대카드Z family',
-    요약: '패밀리용 구간반복 할인카드',
-    설명: ['우리 가족을 위한 쇼핑·마트·배달 10% 할인, 주유 100/L 할인'],
-    요금:['본인 10,000원','본인 10,000원','본인 10,000원'],
+    타이틀: "현대카드Z family",
+    요약: "패밀리용 구간반복 할인카드",
+    설명: ["우리 가족을 위한 쇼핑·마트·배달 10% 할인, 주유 100/L 할인"],
+    요금: ["본인 10,000원", "본인 10,000원", "본인 10,000원"],
+    디자인: [
+      "fam1.png",
+      "fam2.png",
+      "fam3.png",
+      "fam4.png",
+      "fam5.png",
+      "fam6.png",
+      "fam7.png",
+    ],
   },
   mboo: {
-    타이틀: '현대카드 M BOOST',
-    요약: '쓰면 쓸수록 강력한 포인트카드',
-    설명: ['어디서나 한도 없는 최대 4.5% 적립', '페이·해외결제 5% 적립'],
-    요금:['본인 30,000원','본인 30,000원','본인 30,000원'],
+    타이틀: "현대카드 M BOOST",
+    요약: "쓰면 쓸수록 강력한 포인트카드",
+    설명: ["어디서나 한도 없는 최대 4.5% 적립", "페이·해외결제 5% 적립"],
+    요금: ["본인 30,000원", "본인 30,000원", "본인 30,000원"],
+    디자인: [
+      "mboo1.png",
+      "mboo2.png",
+      "mboo3.png",
+      "mboo4.png",
+      "mboo5.png",
+      "mboo6.png",
+      "mboo7.png",
+      "mboo8.png",
+      "mboo9.png",
+    ],
   },
   xboo: {
-    타이틀: '현대카드 X BOOST',
-    요약: '쓰면 쓸수록 강력한 할인카드',
-    설명: ['어디서나 최대 1.5%', '할인 페이·해외결제 5% 할인'],
-    요금:['본인 30,000원','본인 30,000원','본인 30,000원'],
+    타이틀: "현대카드 X BOOST",
+    요약: "쓰면 쓸수록 강력한 할인카드",
+    설명: ["어디서나 최대 1.5%", "할인 페이·해외결제 5% 할인"],
+    요금: ["본인 30,000원", "본인 30,000원", "본인 30,000원"],
+    디자인: [
+      "xboo1.png",
+      "xboo2.png",
+      "xboo3.png",
+      "xboo4.png",
+      "xboo5.png",
+      "xboo6.png",
+      "xboo7.png",
+      "xboo8.png",
+      "xboo9.png",
+    ],
   },
   naver: {
-    타이틀: '네이버 현대카드',
-    요약: '쓸 때마다 네이버페이 포인트 적립받는 카드',
+    타이틀: "네이버 현대카드",
+    요약: "쓸 때마다 네이버페이 포인트 적립받는 카드",
     설명: [
-      '네이버플러스 멤버십 무료 이용권 제공',
-      '네이버플러스 멤버십 적립 대상 결제 시, 네이버페이 포인트 최대 5% 적립',
-      '그 외 가맹점 네이버페이 포인트 1% 적립',
+      "네이버플러스 멤버십 무료 이용권 제공",
+      "네이버플러스 멤버십 적립 대상 결제 시, 네이버페이 포인트 최대 5% 적립",
+      "그 외 가맹점 네이버페이 포인트 1% 적립",
       ,
     ],
-    요금:['본인 10,000원','본인 10,000원']
+    요금: ["본인 10,000원", "본인 10,000원"],
+    디자인: [
+      "naver1.png",
+      "naver2.png",
+      "naver3.png",
+      "naver4.png",
+      "naver5.png",
+      "naver6.png",
+    ],
   },
   black: {
-    타이틀: '혜택의 블랙 라벨',
-    요약: '현대카드 MX Black',
-    설명: ['M의 전 영역 적립과 X의 10% 할인을 동시에'],
-    요금:['본인 150,000원','본인 150,000원','본인 150,000원'],
+    타이틀: "현대카드 MX Black",
+    요약: "혜택의 블랙 라벨",
+    설명: ["M의 전 영역 적립과 X의 10% 할인을 동시에"],
+    요금: ["본인 150,000원", "본인 150,000원", "본인 150,000원"],
+    디자인: [
+      "black1.png",
+      "black2.png",
+      "black3.png",
+      "black4.png",
+      "black5.png",
+      "black6.png",
+    ],
   },
 }; ///////// lang 객체 ///////
